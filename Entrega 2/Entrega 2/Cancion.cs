a961b3d82cff0ce7c8d3c8b133c27284ef3c2ddf
diegoriedel <62268951+diegoriedel@users.noreply.github.com>
2020-03-29 23:22:00 -0300

    Creada public string informacion

---
+++ Entrega 2/Entrega 2/Cancion.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Entrega_2
+{
+    public class Cancion
+    {
+        private string genero;
+        private string artista;
+        private string album;
+        private string nombre;
+        public Cancion(string genero, string artista, string album, string nombre)
+        {
+
+            this.genero = genero;
+            this.artista = artista;
+            this.album = album;
+            this.nombre = nombre;
+        }
+        public string informacion()
+        {
+            return "Genero: " + genero + ", Artista: " + artista + ", Album: " + album + ", Nombre: " + nombre;
+        }
+    }
+}
---
+++ Entrega 2/Entrega 2/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Entrega_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Cancion nombre = new Cancion("Rock","Queen","A night at the opera","Bohemian Rapsody");
+            Console.WriteLine(nombre.informacion());
+
+
+        }
+    }
+}
